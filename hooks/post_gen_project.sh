ba0409ea42772f180cf1c654bac74ec89b275d8c
Cédric HT <altaris@users.noreply.github.com>
2022-02-01 11:10:03 +0900

    Setup git in post gen hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# ==============================================================================
+# Downloads
+# ==============================================================================
+
 function _download () {
     echo "Downloading '$1' to '$2'"
     curl -Ls "$1" -o "$2" > /dev/null
@@ -8,3 +12,11 @@ function _download () {
 _download \
     "https://raw.githubusercontent.com/github/gitignore/main/Python.gitignore" \
     ".gitignore"
+
+# ==============================================================================
+# Git
+# ==============================================================================
+
+git init
+git add ./
+git commit -a -m "Initial commit"
